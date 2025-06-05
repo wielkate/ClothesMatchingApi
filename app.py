ab2c5b51e9eb3e86517a51ea05dbdba1c9c721bd
Ekaterina Mashchenko <mashchenko.kate@gmail.com>
2025-06-05 21:05:21 +0200

    Minimum

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, File, UploadFile
-from rembg import remove
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.post("/remove_bg/")
-async def remove_bg(file: UploadFile = File(...)):
-    output = remove(file.file.read())
-    return {"message": "Background removed", "filename": file.filename, "file": output}
+@app.get("/")
+async def home_root():
+    return {"message": "Success"}
+
+@app.get("/deploy")
+async def home_root():
+    return {"message": "Deployed"}
--- process_image.py
+++
@@ -1,6 +0,0 @@
-from fastapi import UploadFile
-from rembg import remove
-
-
-def remove_bg(file: UploadFile):
-    return remove(file.file.read())
