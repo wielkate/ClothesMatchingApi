f2beab6613cc4b75b761cee1f9e77e0a327b1279
Ekaterina Mashchenko <mashchenko.kate@gmail.com>
2025-06-06 11:50:39 +0200

    Another endpoint

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+import os
+
+from colorthief import ColorThief
 from fastapi import FastAPI, File, UploadFile
-from rembg import remove
 
 app = FastAPI()
 
 
-@app.post("/remove_bg/")
-async def remove_bg(file: UploadFile = File(...)):
+def get_dominant_color_name(file):
+    color_thief = ColorThief(file)
+    dominant_color = color_thief.get_color()
+    return dominant_color
+
+
+@app.post("/do/")
+async def do(file: UploadFile = File(...)):
+    total_memory, used_memory, free_memory = map(
+        int, os.popen('free -t -m').readlines()[-1].split()[1:])
+    print("RAM memory % used:", round((used_memory / total_memory) * 100, 2))
+
     print(f'Get a file named {file.filename} and size {file.size}')
-    output = remove(file.file.read())
+    output = get_dominant_color_name(file.file)
     print('Background removed')
-    return {"message": "Background removed", "filename": file.filename, "file": output}
+    return {"message": "Background removed", "filename": file.filename, "color": output}
 
 
 @app.get("/")
