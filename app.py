a6c1a2bfd377903baafa63095ad1e13ece548e4e
Ekaterina Mashchenko <mashchenko.kate@gmail.com>
2025-06-06 02:18:14 +0200

    Add logs

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = FastAPI()
 
 @app.post("/remove_bg/")
 async def remove_bg(file: UploadFile = File(...)):
+    print(f'Get a file named {file.filename} and size {file.size}')
     output = remove(file.file.read())
+    print('Background removed')
     return {"message": "Background removed", "filename": file.filename, "file": output}
 
 
